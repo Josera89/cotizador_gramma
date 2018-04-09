fe8df0ba09333b9480b8583cb45e62e36ae934e0
Jose Zuniga <jramon.zu@gmail.com>
2018-04-09 18:40:26 -0500

    login auth with frontend

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to '/home'
     else
-      redirect_to '/signup'
+      redirect_to '/error'
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  # get '/landing', to: 'login#index'
   get '/', to: 'sessions#new'
   get '/home', to: 'home#index'
+
+  get '/signup', to: 'users#new'
 
   get '/login', to: 'sessions#new'
   post '/login' => 'sessions#create'
 
   delete 'logout' => 'sessions#destroy'
 
-  get '/signup', to: 'users#new'
+  get '/error' => 'error#index'
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
